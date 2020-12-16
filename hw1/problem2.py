774c709c33fb786a715ea85e4755cd6f67ef5ca4
Roman Itskov <75908271+RomiillST@users.noreply.github.com>
2020-12-16 19:41:31 +0500

    Create problem2.py

---
+++ hw1/problem2.py
@@ -0,0 +1,9 @@
+a=int(input("Введите a: "))
+b=int(input("Введите b: "))
+c=int(input("Введите c: "))
+if c>a and c>b:
+  print(c," самое большое число")
+if a>b and a>c:
+  print(a," самое большое число")
+if b>a and b>c:
+  print(b," самое большое число")
