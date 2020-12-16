260e8df664b004e231351f2a3205724836c8755e
Roman Itskov <75908271+RomiillST@users.noreply.github.com>
2020-12-16 19:41:01 +0500

    Create problem1.py

---
+++ hw1/problem1.py
@@ -0,0 +1,9 @@
+a=int(input("Введите a: "))
+b=int(input("Введите b: "))
+c=int(input("Введите c: "))
+if (a>b or a>c) and (a<b or a<c):
+  print(a," среднее число")
+if (b>a or b>c) and (b<a or b<c):
+  print(b," среднее число")
+if (c>a or c>b) and (c<a or c<b)
+  print(c," среднее число")
