52d2c3a1f8061bdfb50d9024009e5eeccbeadad1
Roman Itskov <75908271+RomiillST@users.noreply.github.com>
2020-12-19 01:32:56 +0500

    Create problem2.py

---
+++ hw3/star/problem2.py
@@ -0,0 +1,118 @@
+import time
+import os
+
+def load():
+    os.system('cls')
+    print('L')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O A')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O A D')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O A D I')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O A D I N')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O A D I N G')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O A D I N G .')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O A D I N G . .')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O A D I N G . . .')
+    time.sleep(0.3)
+    os.system('cls')
+
+
+print('''Привет. Я бот для ленивого Нуридина. Для того, чтобы проверить - Нуриддин ли вы - закончите вашу великую цитату: Лучшая домашка - это ...
+
+1. сделанная вовремя домашка
+2. та, которую оценил Махмуд ака
+3. не заданная домашка
+''')
+answ = int(input('Мой ответ: '))
+if answ == 3:
+    os.system('cls')
+    print('Добро пожаловать мой господин. Чего желаете?')
+    print('''
+    1. Сжать строку, чтобы места меньше занимала.
+    2. Проредить массив чисел, уж больно много там нулей.
+    0. Я незнаю чего я хочу.
+    ''')
+    answ = int(input('Выбираю: '))
+
+    if answ == 1:
+        load()
+        print('Приступаем!')
+        s = 0
+        while True:
+            s = str(input('Введите вашу строку: '))
+            if s == "0" :
+                break
+            count = 1
+            el = s[0]
+            for i in s[1:]:
+                if el[-1] == i:
+                    count += 1
+                else:
+                    el += str(count) + i
+                    count = 1
+            el += str(count)
+            print(el)
+            print("Для завершения работы - введите 0")
+    elif answ == 2:
+        os.system('cls')
+        print('Понимаю. Сейчас оформим.')
+    else:
+        time.sleep(2)
+        os.system('cls')
+        print('''
+
+       ▀   █ █ █▀▀▄ ▄▀▀▄ █   █
+       █   █▀▄ █  █ █  █ █▄█▄█
+       ▀   ▀ ▀ ▀  ▀  ▀▀   ▀ ▀
+
+    ▀█▀ █ █ ▄▀▄ ▀█▀  █▀ █▀ █▀ █   █▀▄ █▀▄ ▄▀▄
+     █  █▀█ █▄█  █   █▀ █▀ █▀ █   █▀▄ █▄▀ █ █
+     ▀  ▀ ▀ ▀ ▀  ▀   ▀  ▀▀ ▀▀ ▀▀  ▀▀  ▀ ▀  ▀   
+                          ▄█▀▀▀▀▀█▄
+                ▄▄▄▄▄   █▀        ▀█
+               █    ▀█▄█           ▀█
+             ██       █      ▄▄      █
+            █          █ ▄▄▀▀  ▀▀▀▀   █
+           █           █    ▀▀▄▄▄▀▀   █
+          ▐             █             █
+          ▐             █ ▀▀▀▄    ▄   █
+          ▐             █     ▀  ▀ ▀▄ █
+          ▐             █ ███▄        █
+          ▐             █    ▀    ██ ▐
+          ▐             █       ▄     █▄▄
+          ▐             █         ▌   ██ ▌
+          ▐             █   ▄▀ ▄ ▌   █   ▌
+           █            █    ▀  ▀   █    ▌
+            █            █   ▄▄▄▄  █     ▌
+          ▄▀▀▄   ▌       ██       █▌     ▌
+        ▄▀    █  ▌        ▀▀█████▀▀      ▌
+      ▄▀       ███▌         ▌            ▌
+    ▄▀            █                     █
+    █       ▄██▀▀▀▀                     ▌
+    █     ▄██▄                          ▌
+    █        ▀▄      ▄▀▀▀▀▄            █
+    █          ▀▄  ▄▀  ▄▄▄▀            ▌
+    ▀▄           ▀▀    █               ▌
+    █ ▀▄                ▀▀▀▀▀▄         ▌
+    █   ▀▄▄               ▄▄▄▀         ▌''')
+else:
+    os.system('cls')
+    print("Мдааа... Не для вас мои коды писались. Брысь!")
