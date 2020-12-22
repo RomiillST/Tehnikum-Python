e86c07664c56f006070aa89c634072f5f441ff2d
Roman Itskov <75908271+RomiillST@users.noreply.github.com>
2020-12-22 19:37:17 +0500

    Create problem2.py

---
+++ hw3/star/problem2.py
@@ -0,0 +1,137 @@
+
+import time
+import os
+
+def stroka():
+    while True:
+       s = input()
+        a = 0
+        b = s[0]
+        result = ""
+        for i in s:
+            if i == b:
+                a += 1
+            else:
+                result += b + str(a)                    
+                a = 1
+                b = i
+        result += b + str(a)
+        print(result)
+        print("Для завершения работы - введите 0")
+
+def zero():
+    while True:
+        z = []
+        s = str(input('Введите ряд чисел( через пробел ): '))
+        if s == '*':
+            break
+        s = s.split(' ')
+        l = len(s)
+        for x in s:
+            if x == '0':
+                z.append(x)
+                s.remove(x)
+        z = s + z
+        print(z)
+        print('Для завершения работы архиватора - введите *.')
+
+def load():
+    os.system('cls')
+    print('L')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O A')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O A D')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O A D I')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O A D I N')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O A D I N G')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O A D I N G .')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O A D I N G . .')
+    time.sleep(0.3)
+    os.system('cls')
+    print('L O A D I N G . . .')
+    time.sleep(0.3)
+    os.system('cls')
+
+
+print('''Привет. Я бот для ленивого Нуридина. Для того, чтобы проверить - Нуриддин ли вы - закончите вашу великую цитату: Лучшая домашка - это ...
+1. сделанная вовремя домашка
+2. та, которую оценил Махмуд ака
+3. не заданная домашка
+''')
+answ = int(input('Мой ответ: '))
+if answ == 3:
+    os.system('cls')
+    print('Добро пожаловать мой господин. Чего желаете?')
+    print('''
+    1. Сжать строку, чтобы места меньше занимала.
+    2. Проредить массив чисел, уж больно много там нулей.
+    0. Я незнаю чего я хочу.
+    ''')
+    answ = int(input('Выбираю: '))
+
+    if answ == 1:
+        load()
+        print('Приступаем!')
+        s = 0
+        stroka()
+    elif answ == 2:
+        os.system('cls')
+        load()
+        zero()
+        print('Спасибо за пользование моими функциями Нуриддин!')
+    else:
+        time.sleep(2)
+        os.system('cls')
+        print('''
+       ▀   █ █ █▀▀▄ ▄▀▀▄ █   █
+       █   █▀▄ █  █ █  █ █▄█▄█
+       ▀   ▀ ▀ ▀  ▀  ▀▀   ▀ ▀
+    ▀█▀ █ █ ▄▀▄ ▀█▀  █▀ █▀ █▀ █   █▀▄ █▀▄ ▄▀▄
+     █  █▀█ █▄█  █   █▀ █▀ █▀ █   █▀▄ █▄▀ █ █
+     ▀  ▀ ▀ ▀ ▀  ▀   ▀  ▀▀ ▀▀ ▀▀  ▀▀  ▀ ▀  ▀   
+                          ▄█▀▀▀▀▀█▄
+                ▄▄▄▄▄   █▀        ▀█
+               █    ▀█▄█           ▀█
+             ██       █      ▄▄      █
+            █          █ ▄▄▀▀  ▀▀▀▀   █
+           █           █    ▀▀▄▄▄▀▀   █
+          ▐             █             █
+          ▐             █ ▀▀▀▄    ▄   █
+          ▐             █     ▀  ▀ ▀▄ █
+          ▐             █ ███▄        █
+          ▐             █    ▀    ██ ▐
+          ▐             █       ▄     █▄▄
+          ▐             █         ▌   ██ ▌
+          ▐             █   ▄▀ ▄ ▌   █   ▌
+           █            █    ▀  ▀   █    ▌
+            █            █   ▄▄▄▄  █     ▌
+          ▄▀▀▄   ▌       ██       █▌     ▌
+        ▄▀    █  ▌        ▀▀█████▀▀      ▌
+      ▄▀       ███▌         ▌            ▌
+    ▄▀            █                     █
+    █       ▄██▀▀▀▀                     ▌
+    █     ▄██▄                          ▌
+    █        ▀▄      ▄▀▀▀▀▄            █
+    █          ▀▄  ▄▀  ▄▄▄▀            ▌
+    ▀▄           ▀▀    █               ▌
+    █ ▀▄                ▀▀▀▀▀▄         ▌
+    █   ▀▄▄               ▄▄▄▀         ▌''')
+else:
+    os.system('cls')
+    print("Мдааа... Не для вас мои коды писались. Брысь!")
