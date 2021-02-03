b5aba6a239b57c764000ca6d7ca3b24aa873ebd6
Roman Itskov <75908271+RomiillST@users.noreply.github.com>
2021-02-03 16:45:39 +0500

    Delete final_bot directory

--- final_bot/req.py
+++
@@ -1,6 +0,0 @@
-import requests
-sms_token = 'Roman:918812496063'
-URL = 'https://api.tehnikum.school/sms/sms.php'
-requests.post(URL, data={'token' : sms_token,
-                        'sms_phone' : '{}',
-                        'sms_text' : '{}'})
